0edd64b4dacd4c870b8857189cf2f71bb934fd74
Woolee Han <woha5678@colorado.edu>
2024-11-06 09:46:03 -0700

    added only one user for testing

---
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO buffspace_main.user
+  (user_id, username, password, created_at, last_login)
+VALUES
+  (
+	1,
+    'orange',
+    'abcdefg',
+    '2024-06-24 12:34:56', 
+    '2024-06-24 12:34:56'
+  );
