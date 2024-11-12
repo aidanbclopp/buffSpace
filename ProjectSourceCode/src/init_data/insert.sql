fb8d7f8098e8f668beb64399dcbdbaf8e36404c2
Woolee Han <woha5678@colorado.edu>
2024-11-12 12:16:37 -0700

    modified insert data

--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -1,11 +1,11 @@
 INSERT INTO buffspace_main.user
-  (user_id, username, password, created_at, last_login)
+  (username, password, confirm_password, created_at, last_login)
 VALUES
   (
-	1,
     'orange',
     'abcdefg',
-    '2024-06-24 12:34:56', 
+    'abcdefg',
+    '2024-06-24 12:34:56',
     '2024-06-24 12:34:56'
   );
 
